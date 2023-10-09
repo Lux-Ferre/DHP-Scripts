c2f7c75aaef33cfc1dcd0cbed3f29614548e166e
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2023-10-09 11:40:19 +0100

    db link remover
    IP+ Plugin that removes the function that turns words in chat into links to the database, discord, wiki etc

---
+++ remove-links.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         IdlePixel Database Link Remover
+// @namespace    lbtechnology.info
+// @version      1.0.0
+// @description  Stops items being turned into database etc links in chat
+// @author       Lux-Ferre
+// @license      MIT
+// @match        *://idle-pixel.com/login/play*
+// @grant        none
+// @require      https://greasyfork.org/scripts/441206-idlepixel/code/IdlePixel+.js?anticache=20220905
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    class SigilPlugin extends IdlePixelPlusPlugin {
+        constructor() {
+            super("nolinks", {
+                about: {
+                    name: GM_info.script.name,
+                    version: GM_info.script.version,
+                    author: GM_info.script.author,
+                    description: GM_info.script.description
+                }
+            });
+            this.previous = "";
+        }
+
+    onLogin(){
+            SearchAnythingData.create_link_for_chat = (data) => {return data}
+        }
+    }
+
+    const plugin = new SigilPlugin();
+        IdlePixelPlus.registerPlugin(plugin);
+    
+})(); 
