1b61c4260e726ac4139c0a5500ea0545c49457cc
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2023-11-20 19:16:49 +0000

    template: added standard handling for customs

--- template.js
+++ template.js
@@ -2,7 +2,7 @@
 // @name         IdlePixel+ Plugin Template
 // @namespace    lbtechnology.info
 // @version      1.0.0
-// @description  Blank plugin with empty methods 
+// @description  Blank plugin with all IP+ methods, and custom message handling
 // @author       Lux-Ferre
 // @license      MIT
 // @match        *://idle-pixel.com/login/play*
@@ -43,9 +43,63 @@
         onPanelChanged(panelBefore, panelAfter) { }
         onCombatStart() { }
         onCombatEnd() { }
-        onCustomMessageReceived(player, content, callbackId) { }
         onCustomMessagePlayerOffline(player, content) { }
+        onCustomMessageReceived(player, content, callbackId) {
+            const customData = this.parseCustom(player, content, callbackId)        // Parses custom data into an object, assumes the Anwinity Standard
+            if (!(customData.plugin === "--template--" || customData.anwinFormatted)){      // Checks if custom is formatted in the correct way, and from the correct plugin
+                return
+            }
+            if (customData.player === "--template--"){      // Checks if custom is received from the correct player
+                if (customData.command === "--template--"){     // Runs relevant command code, replace with switch statment if using many commands
+                    console.log(customData.payload)
+                }
+            }
+        }
     
+         sendCustom(){
+            const recipient = ""
+            const pluginValue = ""
+            const command = ""
+            const data = ""
+
+            const content = `${pluginValue}:${command}:${data}`
+
+            const payload = {
+                content: content,
+                onResponse: function(player, content, callbackId) {
+                        return true;
+                },
+                onOffline: function(player, content) {
+                        console.log(content)
+                },
+                timeout: 2000 // callback expires after 2 seconds
+            }
+
+            IdlePixelPlus.sendCustomMessage(recipient, payload)
+        }
+
+        parseCustom(player, content, callbackId){
+            const customData = {
+                player: player,
+                callbackId: callbackId,
+                anwinFormatted: false
+            }
+            const splitPayload = content.split(":")
+            if(splitPayload.length >= 3){
+                customData.anwinFormatted = true
+                customData.plugin = splitPayload[0]
+                customData.command = splitPayload[1]
+                customData.payload = splitPayload.slice(2).join(":")
+            } else {
+                customData.anwinFormatted = false
+                customData.plugin = "unknown"
+                customData.command = "unknown"
+                customData.payload = content
+            }
+
+            return customData
+        }
+
     }
 
     const plugin = new TemplatePlugin();
