7c662500f4565c8a8bcee6e809b9bb0b53c90502
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2023-12-23 11:51:00 +0000

    clammy-vock: new script
    Replaces "Clammy" with a randomly generated name

---
+++ clammy-vock.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         IdlePixel+ Clammy Vock
+// @namespace    lbtechnology.info
+// @version      1.0.0
+// @description  Replaces the word Clammy with a randomly generated name when sending chat messages.
+// @author       Lux-Ferre
+// @license      MIT
+// @match        *://idle-pixel.com/login/play*
+// @grant        none
+// @require      https://greasyfork.org/scripts/441206-idlepixel/code/IdlePixel+.js?anticache=20220905
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	class ClammyPlugin extends IdlePixelPlusPlugin {
+		constructor() {
+			super("clammy", {
+				about: {
+					name: GM_info.script.name,
+					version: GM_info.script.version,
+					author: GM_info.script.author,
+					description: GM_info.script.description
+				}
+			});
+			this.previous = "";
+		}
+
+		onLogin() {
+			this.firstNames = ["Cabin", "Cable", "Calendar", "Camera", "Cancel", "Candle", "Canvas", "Captain", "Capture", "Carbon", "Carpeted", "Carriage", "Carrot", "Cinema", "Circular", "Citizen", "Compacted", "Company", "Complete", "Compound", "Computer", "Comprehensive", "Concept", "Concrete", "Conductor", "Confined", "Conflict", "Copper", "Costume", "Cottage", "Cotton", "Council", "Counter", "Country", "Couple", "Courtesy", "Curtsy", "Coverage", "Cassette", "Castle", "Casualty", "Category", "Cathederal", "Cemetery", "Censorship", "Century", "Cereal", "Ceremony", "Champion", "Character", "Ceiling", "Charity", "Climate", "Coincide", "Collapsed", "Collected", "Colourful", "Combination", "Comedic", "Comfortable", "Confused", "Constructed", "Constitutional", "Contradictory", "Crosswalk", "Chauvinist", "Cylidrical", "Covfefe", "Caustic"]
+			this.lastNames = ["Block", "Caulk", "Chalk", "Croc", "Dock", "Frock", "Gawk", "Hawk", "Jock", "Lock", "Mock", "Schlock", "Sock", "Stock", "Stonk", "Wok", "Vock", "Tock", "Knock"]
+			
+			$("#game-chat .m-2 .m-2 button:not(.btn-chat-configure)").attr("onClick", "IdlePixelPlus.plugins.clammy.sendChat()")
+		}
+	
+		choose(inArray){
+			return inArray[Math.floor((Math.random()*inArray.length))];
+		}
+	
+		sendChat(){
+			const fullName = `${this.choose(this.firstNames)} ${this.choose(this.lastNames)}`
+			
+			document.getElementById("chat-area-input").value = document.getElementById("chat-area-input").value.replace("Clammy", fullName);
+			Chat.send()
+		}
+	}
+
+	const plugin = new ClammyPlugin();
+	IdlePixelPlus.registerPlugin(plugin);
+
+})();
