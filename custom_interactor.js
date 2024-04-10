f742b06103d710bfeeb09c1a693693ecbdfd84d9
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-04-10 16:19:28 +0100

    interactor: modified paneller require url

--- custom_interactor.js
+++ custom_interactor.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         IdlePixel Custom Interactor
 // @namespace    lbtechnology.info
-// @version      2.3.1
+// @version      2.3.2
 // @description  Sends, receives, and displays CUSTOM websocket frames 
 // @author       Lux-Ferre
 // @license      MIT
 // @match        *://idle-pixel.com/login/play*
 // @grant        none
 // @require      https://greasyfork.org/scripts/441206-idlepixel/code/IdlePixel+.js?anticache=20220905
-// @require		 https://greasyfork.org/en/scripts/491983-idlepixel-plugin-paneller/code/IdlePixel%2B%20Plugin%20Paneller.js
+// @require		 https://greasyfork.org/scripts/491983-idlepixel-plugin-paneller/code/IdlePixel%2B%20Plugin%20Paneller.js?anticache=20240410
 // ==/UserScript==
  
 (function() {
