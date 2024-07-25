11196d0018ac2c49ad4a6c4cfcd051ca60ad3a21
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-07-25 11:24:13 +0100

    llibs: new anticache

--- lux_libs.js
+++ lux_libs.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name			Idle-Pixel - Lux Libs
 // @namespace		luxferre.dev
-// @version			1.0.1
+// @version			1.0.2
 // @description		Loader for Lux's Libraries
 // @author			Lux-Ferre
 // @license			MIT
 // @match			*://idle-pixel.com/login/play*
 // @grant			none
-// @require			https://greasyfork.org/scripts/484046/code/IdlePixel%2B%20Custom%20Handling.js?anticache=20240724
-// @require			https://greasyfork.org/scripts/491983-idlepixel-plugin-paneller/code/IdlePixel%2B%20Plugin%20Paneller.js?anticache=20240724
-// @require			https://greasyfork.org/scripts/501360/code/Idle-Pixel%20Teams%20Storage%20Manager.js?anticache=20240724
+// @require			https://greasyfork.org/scripts/484046/code/IdlePixel%2B%20Custom%20Handling.js?anticache=20240725
+// @require			https://greasyfork.org/scripts/491983-idlepixel-plugin-paneller/code/IdlePixel%2B%20Plugin%20Paneller.js?anticache=20240725
+// @require			https://greasyfork.org/scripts/501360/code/Idle-Pixel%20Teams%20Storage%20Manager.js?anticache=20240725
 // ==/UserScript==
 
 (function() {
